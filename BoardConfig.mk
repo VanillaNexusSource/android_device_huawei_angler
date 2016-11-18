97b760e9d8e8f761501f29b39ee3d3380ea1d875
mydongistiny <jaysonedson@gmail.com>
2016-11-17 21:01:01 -0800

    angler: We don't want your perf script
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,9 +135,6 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 #NFC
 NXP_CHIP_TYPE := 2
 
-# Testing related defines
-BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/angler-setup.sh
-
 USE_CLANG_PLATFORM_BUILD := true
 
 -include vendor/huawei/angler/BoardConfigVendor.mk
